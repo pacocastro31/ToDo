106c55734e3f415c58ee86ad0850296d56d7742e
unknown <pacocastro608@gmail.com>
2019-02-02 03:09:43 -0600

    Lab 2.0

---
+++ js/javascript.js
@@ -0,0 +1,30 @@
+var ul = document.getElementById("lista");
+ul.addEventListener("click",function(ev){
+  var element = ev.target;
+  if(element.type === "checkbox"){
+    element.nextSibling.classList.toggle("done");
+  }
+});
+
+let checkboxes = document.getElementsByName("todo");
+let input = document.getElementById("newitem");
+input.addEventListener("keyup", function(event) {
+    if (event.key === "Enter") {
+
+        var node = document.createElement("LI");
+        var span = document.createElement("span");
+        var newCheckBox = document.createElement("input");
+        newCheckBox.type = "checkbox";
+        newCheckBox.name = "todo";
+        newCheckBox.value = checkboxes.length;
+
+        var textNode =document.createTextNode(newitem.value);
+        node.appendChild(newCheckBox);
+        node.appendChild(span);
+        span.appendChild(textNode);
+        
+
+        document.getElementById("lista").appendChild(node);
+        newitem.value = "";
+    }
+});
